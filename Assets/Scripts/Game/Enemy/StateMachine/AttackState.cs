94b5560be5f4d6805acd227fa017c78e59a75f3a
Pavel <oavelasdqwe8@gmail.com>
2022-12-25 02:05:51 +0300

    fixes

---
+++ Assets/Scripts/Game/Enemy/StateMachine/AttackState.cs
@@ -0,0 +1,38 @@
+﻿using Game.Enemy.Attack.Abstract;
+using Game.StateMachine.Abstract;
+using Game.StateMachine.Interfaces;
+
+namespace Game.Enemy.StateMachine
+{
+	public class AttackState : State
+	{
+		private readonly AbstractAttack _attack;
+
+		public AttackState(AbstractAttack attack, IStateMachine stateMachine) : base(stateMachine)
+		{
+			_attack = attack;
+		}
+		
+		public override void Enter()
+		{
+
+		}
+
+		public override void Update(float deltaTime)
+		{
+			if (_attack.InCoolDown)
+			{
+				_attack.CurrentCoolDown += deltaTime;
+			}
+			else
+			{
+				_attack.CurrentCoolDown = 0;
+				_attack.Attack();
+			}
+		}
+
+		public override void Exit()
+		{
+		}
+	}
+}
---
+++ Assets/Scripts/Game/Enemy/StateMachine/MovementState.cs
@@ -0,0 +1,35 @@
+﻿using Game.Enemy.Movement.Abstract;
+using Game.StateMachine.Abstract;
+using Game.StateMachine.Interfaces;
+
+namespace Game.Enemy.StateMachine
+{
+	public class MovementState : State
+	{
+		private readonly AbstractMovement _movement;
+		public MovementState(AbstractMovement movement, IStateMachine stateMachine) : base(stateMachine)
+		{
+			_movement = movement;
+		}
+		
+		public override void Enter()
+		{
+			
+		}
+		
+		public override void Update(float deltaTime)
+		{
+			if (_movement.ReachedTarget)
+			{
+				StateMachine.ChangeState<AttackState>();
+				return;
+			}
+			
+			_movement.Move(deltaTime);			
+		}
+		
+		public override void Exit()
+		{
+		}
+	}
+}
